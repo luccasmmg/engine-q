d06240028e2b1d1ec8eb30c8973d17d2e2672cd9
Luccas Mateus de Medeiros Gomes <luccasmmg@gmail.com>
2021-11-05 10:45:46 -0300

    MathVariance, MathEval and MathStddev

--- crates/nu-command/src/math/mod.rs
+++ crates/nu-command/src/math/mod.rs
@@ -1,21 +1,27 @@
 mod abs;
 mod avg;
 pub mod command;
+mod eval;
 mod max;
 mod min;
 mod product;
 mod reducers;
 mod round;
 mod sqrt;
+mod stddev;
 mod sum;
+mod variance;
 mod utils;
 
 pub use abs::SubCommand as MathAbs;
 pub use avg::SubCommand as MathAvg;
 pub use command::MathCommand as Math;
+pub use eval::SubCommand as MathEval;
 pub use max::SubCommand as MathMax;
 pub use min::SubCommand as MathMin;
 pub use product::SubCommand as MathProduct;
 pub use round::SubCommand as MathRound;
 pub use sqrt::SubCommand as MathSqrt;
+pub use stddev::SubCommand as MathStddev;
 pub use sum::SubCommand as MathSum;
+pub use variance::SubCommand as MathVariance;
